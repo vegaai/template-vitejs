198c6c1b40e4e6e239886a28d4bf79d41afde34b
Imjuliio <jc7104484@gmail.com>
2025-12-02 18:26:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+
+ 
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the App.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vite.dev/guide/"
+              className="font-medium text-zinc-950 dark:text-zinc-50 hover:underline"
+            >
+              Vite Guide
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://vite.dev/guide/features.html"
+              className="font-medium text-zinc-950 dark:text-zinc-50 hover:underline"
+            >
+              Features
+            </a>{" "}
+            page.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-zinc-950 px-5 text-white transition-colors hover:bg-[#383838] dark:bg-zinc-50 dark:text-zinc-950 dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vite.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            
+            Learn More
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://vite.dev/guide/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
